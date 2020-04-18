c1f2a372a24da1c76588008aff414da0b5e20412
osiaeg <egor.goncharov.osia@mail.ru>
2020-04-18 12:42:53 +0300

    Add install script for vim

---
+++ install_vim.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+sudo pacman -Syu vim;
+git clone https://github.com/osiaeg/config_files.git;
+cd config_files/;
+cp .vimrc ~/;
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
+
+echo "alias='ls -alh'" >> ~/.bashrc
+source ~/.bashrc
