cb713019dce22bb435a78ec938d771bf84cd8201
Егор Гончаров <eg@calculate.ru>
2024-08-02 09:30:45 +0300

    Close tabs

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -4,7 +4,9 @@ vim.g.mapleader = "\\"
 -- NeoTree
 vim.keymap.set('n', '<leader>n', ':Neotree float focus <Enter>')
 vim.keymap.set('n', '<leader>g', ':Neotree float git_status <Enter>')
-vim.keymap.set('n', '<leader>t', ':ToggleTerm dir=./ direction=float name=term<Enter>')
+vim.keymap.set('n', '<C-q>', ':bd <Enter>')
+vim.keymap.set('n', '<leader>t', ':ToggleTerm dir=./ direction=float name=Terminal<Enter>')
+
 
 
  
